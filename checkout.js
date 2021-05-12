b58cfcb6f5833783e94b930d15c01fc4daefb043
Mayankyadav3980 <44013964+Mayankyadav3980@users.noreply.github.com>
2021-05-12 18:03:56 +0530

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -2,21 +2,21 @@
 <script src="https://checkout.razorpay.com/v1/checkout.js"></script>
 <script>
 var options = {
-    "key": "YOUR_KEY_ID", // Enter the Key ID generated from the Dashboard
+    "key": "YOUR_KEY_ID", 
     "amount": "50000", // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
     "currency": "INR",
     "name": "Acme Corp",
     "description": "Test Transaction",
     "image": "https://example.com/your_logo",
-    "order_id": "order_9A33XWu170gUtm", //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+    "order_id": "order_9A33XWu170gUtm", 
     "handler": function (response){
         alert(response.razorpay_payment_id);
         alert(response.razorpay_order_id);
         alert(response.razorpay_signature)
     },
     "prefill": {
-        "name": "Gaurav Kumar",
-        "email": "gaurav.kumar@example.com",
+        "name": "Mayank Yadav",
+        "email": "abc@example.com",
         "contact": "9999999999"
     },
     "notes": {
